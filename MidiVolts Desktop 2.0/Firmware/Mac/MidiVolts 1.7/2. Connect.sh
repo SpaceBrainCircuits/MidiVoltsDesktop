b5ff116564d426a50cc7a680f6009875f810bbe5
Austin Luberda <austinluberda@gmail.com>
2026-02-17 19:40:05 -0600

    added mac fw files

---
+++ MidiVolts Desktop 2.0/Firmware/Mac/MidiVolts 1.7/2. Connect.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+screen /dev/tty.usbmodem1A12101 1200
---
+++ MidiVolts Desktop 2.0/Firmware/Mac/MidiVolts Split 1.3/3. Upload.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+./avrdude -C./avrdude.conf -v -D -patmega32u4 -cavr109 -P/dev/tty.usbmodem1A12101 -b57600 -Uflash:w:./MidiVoltsSplit1.3.hex:i
